e5309d4cddd5d3a92e56e77b6d200c59c150298f
maple-johnson <maplej678@gmail.com>
2023-06-09 18:16:18 -0700

    Added documentation to the project.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+/**
+ * This is the entry point to my sample app.
+ *
+ * @author Maple Johnson
+ * @version 1.0
+ */
 public class Main
 {
+    /**
+     * Starts the application
+     * @param args command-line args (not used)
+     */
     public static void main(String[] args)
     {
         System.out.println("Hello world, from SDEV 220!");
